bc9adbd2b0c6b8ccc0f93f2d3159a3767bc0a019
amohan179 <157762058+amohan179@users.noreply.github.com>
2025-05-31 11:11:23 -0400

    Make Project

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from "@mui/material/Button"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+
+<Button variant="contained">Testing</Button>
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
